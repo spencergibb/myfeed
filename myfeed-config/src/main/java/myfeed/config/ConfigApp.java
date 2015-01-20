f7df8261542988b1a2f79b419981b8149f2c4011
Spencer Gibb <spencer@gibb.us>
2015-01-19 17:01:10 -0700

    added configserver

---
+++ myfeed-config/src/main/java/myfeed/config/ConfigApp.java
@@ -0,0 +1,18 @@
+package myfeed.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author Spencer Gibb
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class ConfigApp {
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigApp.class, args);
+	}
+}
