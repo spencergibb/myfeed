bd459fffb6e65fdcfab1df98aaf7c04e8482cb63
Spencer Gibb <spencer@gibb.us>
2015-01-30 15:54:11 -0700

    added github sso to ui

--- myfeed-ui/src/main/java/myfeed/ui/UiApp.java
+++ myfeed-ui/src/main/java/myfeed/ui/UiApp.java
@@ -3,22 +3,104 @@ package myfeed.ui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.security.oauth2.sso.EnableOAuth2Sso;
+import org.springframework.cloud.security.oauth2.sso.OAuth2SsoConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.security.Principal;
+import java.util.Collections;
+import java.util.Map;
 
 /**
  * @author Spencer Gibb
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableOAuth2Sso
 @RestController
+@RequestMapping("/dashboard")
 public class UiApp {
+
+	@RequestMapping("/message")
+	public Map<String, Object> dashboard() {
+		return Collections.<String, Object> singletonMap("message", "Yay!");
+	}
+
+	@RequestMapping("/user")
+	public Principal user(Principal user) {
+		return user;
+	}
 
 	@RequestMapping("/")
 	public String home() {
 		return "Hello myfeed";
 	}
 
+	@Controller
+	public static class LoginErrors {
+
+		@RequestMapping("/dashboard/login")
+		public String dashboard() {
+			return "redirect:/#/";
+		}
+
+	}
+	@Component
+	public static class LoginConfigurer extends OAuth2SsoConfigurerAdapter {
+
+		@Override
+		public void match(RequestMatchers matchers) {
+			matchers.antMatchers("/dashboard/**");
+		}
+
+		@Override
+		public void configure(HttpSecurity http) throws Exception {
+			http.antMatcher("/dashboard/**").authorizeRequests().anyRequest()
+					.authenticated().and().csrf()
+					.csrfTokenRepository(csrfTokenRepository()).and()
+					.addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
+		}
+
+		private Filter csrfHeaderFilter() {
+			return new OncePerRequestFilter() {
+				@Override
+				protected void doFilterInternal(HttpServletRequest request,
+												HttpServletResponse response, FilterChain filterChain)
+						throws ServletException, IOException {
+					CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class
+							.getName());
+					if (csrf != null) {
+						Cookie cookie = new Cookie("XSRF-TOKEN", csrf.getToken());
+						cookie.setPath("/");
+						response.addCookie(cookie);
+					}
+					filterChain.doFilter(request, response);
+				}
+			};
+		}
+
+		private CsrfTokenRepository csrfTokenRepository() {
+			HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+			repository.setHeaderName("X-XSRF-TOKEN");
+			return repository;
+		}
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(UiApp.class, args);
 	}
