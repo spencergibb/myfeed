6171dfb730d7ed340ded483ae77777f10e80c365
Spencer Gibb <spencer@gibb.us>
2015-02-05 16:21:11 -0700

    closer to good security

--- myfeed-ui/src/main/java/myfeed/ui/UiApp.java
+++ myfeed-ui/src/main/java/myfeed/ui/UiApp.java
@@ -10,6 +10,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.security.oauth2.resource.EnableOAuth2Resource;
+import org.springframework.cloud.security.oauth2.sso.OAuth2SsoConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,23 +54,21 @@ public class UiApp {
 		}
 	}
 
-/*	@Component
+	@Component
 	public static class LoginConfigurer extends OAuth2SsoConfigurerAdapter {
 
 		@Override
 		public void match(RequestMatchers matchers) {
-			matchers.antMatchers("/dashboard*//**");
+			matchers.antMatchers("/dashboard/**");
 		}
 
 		@Override
 		public void configure(HttpSecurity http) throws Exception {
-			http.antMatcher("/dashboard*//**").authorizeRequests().anyRequest()
-					.authenticated().and().csrf()
-					.csrfTokenRepository(csrfTokenRepository()).and()
-					.addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
+			http.antMatcher("/dashboard/**").authorizeRequests().anyRequest()
+					.authenticated();
 		}
 
-		private Filter csrfHeaderFilter() {
+		/*private Filter csrfHeaderFilter() {
 			return new OncePerRequestFilter() {
 				@Override
 				protected void doFilterInternal(HttpServletRequest request,
@@ -88,8 +89,8 @@ public class UiApp {
 			HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
 			repository.setHeaderName("X-XSRF-TOKEN");
 			return repository;
-		}
-	}*/
+		}*/
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UiApp.class, args);
