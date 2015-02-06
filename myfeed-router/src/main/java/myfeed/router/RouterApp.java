96c6f609119ff42cf643b62ccbb1b59a46467c72
Dave Syer <dsyer@pivotal.io>
2015-02-06 16:49:17 +0000

    Fix logout by ensuring security filter is in the right place
    The RouterApp needs to be sending CSRF tokens as cookies for logout
    (or any POST) to work.
    
    There is a filter installed by @EnableOAuthSso and you need to configure
    *that* one if you want to get the cookie sent, which means using
    OAuth2SsoConfigurerAdapter instead of WebSecurityConfigurerAdapter.

--- myfeed-router/src/main/java/myfeed/router/RouterApp.java
+++ myfeed-router/src/main/java/myfeed/router/RouterApp.java
@@ -1,21 +1,6 @@
 package myfeed.router;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.security.oauth2.sso.EnableOAuth2Sso;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.security.web.csrf.CsrfToken;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
-import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.util.WebUtils;
+import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -23,29 +8,45 @@ import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.security.oauth2.sso.EnableOAuth2Sso;
+import org.springframework.cloud.security.oauth2.sso.OAuth2SsoConfigurerAdapter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
 
 /**
  * @author Spencer Gibb
  */
 @SpringBootApplication
 @EnableZuulProxy
-@EnableDiscoveryClient
-//@EnableRedisHttpSession
 @EnableOAuth2Sso
 public class RouterApp {
+
 	public static void main(String[] args) {
 		SpringApplication.run(RouterApp.class, args);
 	}
-
-
+	
 	@Configuration
-	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	protected static class SecurityConfiguration extends OAuth2SsoConfigurerAdapter {
+		
 		@Override
-		protected void configure(HttpSecurity http) throws Exception {
+		public void match(RequestMatchers matchers) {
+			matchers.anyRequest();
+		}
+		
+		@Override
+		public void configure(HttpSecurity http) throws Exception {
 			http.authorizeRequests()
-					.antMatchers("/**/*.html", "/js/**", "/css/**", "/webjars/**", "/index.html", "/home.html", "/").permitAll().anyRequest()
+					.antMatchers("/").permitAll().anyRequest()
 					.authenticated().and().csrf()
 					.csrfTokenRepository(csrfTokenRepository()).and()
 					.addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
