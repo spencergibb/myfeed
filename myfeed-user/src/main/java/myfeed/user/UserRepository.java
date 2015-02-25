b8ddba002f59aff2f9b3d9175c7364878daca762
Spencer Gibb <spencer@gibb.us>
2015-02-25 15:19:10 -0700

    added findByFollowing

--- myfeed-user/src/main/java/myfeed/user/UserRepository.java
+++ myfeed-user/src/main/java/myfeed/user/UserRepository.java
@@ -1,12 +1,16 @@
 package myfeed.user;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 /**
  * @author Spencer Gibb
  */
 public interface UserRepository extends PagingAndSortingRepository<User, String> {
 
-	public User findByUsername(String username);
-	public String deleteByUsername(String username);
+	public User findByUsername(@Param("username") String username);
+	public String deleteByUsername(@Param("username") String username);
+	public List<User> findByFollowing(@Param("userId") String userId);
 }
